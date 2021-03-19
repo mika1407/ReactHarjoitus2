ce632cffbecec9c215e70b807ec816487dd7d9e7
mika1407 <koneautomaatio@hotmail.com>
2021-03-19 18:06:28 +0200

    LoginEdit lisätty backend puuttuu, LoginAdd tekemättä

---
+++ src/logins/LoginEdit.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react'
+import '../App.css'
+import LoginService from '../services/logins'
+
+const LoginEdit = ({ setMuokkaustila, setLogins, logins, setMessage, setShowMessage,
+    setIsPositive, muokattavaLogin }) => {
+
+    // State määritykset
+
+    const [newLoginId, setNewLoginId] = useState(muokattavaLogin.loginId)
+    const [newUsername, setNewUsername] = useState(muokattavaLogin.username)
+    const [newPassword, setNewPassword] = useState(muokattavaLogin.password)
+    const [newFirstname, setNewFirstname] = useState(muokattavaLogin.firstname)
+
+    const [newLastname, setNewLastname] = useState(muokattavaLogin.lastname)
+    const [newEmail, setNewEmail] = useState(muokattavaLogin.email)
+    const [newAccesslevelID, setNewAccesslevelID] = useState(muokattavaLogin.accesslevelid)
+    
+
+    // Muokkauslomakkeen onSubmit tapahtumankäsittelijä
+
+    const submitLogin = (event) => {
+        event.preventDefault()
+        var changedLogin = {
+            loginId: newLoginId.toUpperCase(),
+            username: newUsername,
+            password: newPassword,
+            firstname: newFirstname,
+            lastname: newLastname,
+            email: newEmail,
+            accesslevelid: newAccesslevelID 
+        }
+
+        LoginService
+            .update(changedLogin) // Put pyyntö back-endille
+            .then(response => {
+
+                if (response.status === 200) {
+
+                    const id = changedLogin.loginId
+
+                    // Poistetaan ensin vanha login statesta
+                    setLogins(logins.filter(filtered => filtered.loginId !== id))
+
+                    // Ja lisätään uudestaan muuttuneilla tiedoilla
+                    setLogins(logins.concat(changedLogin))
+
+                    setMessage(`Päivitetty ${changedLogin.firstname}`)
+                    setIsPositive(true)
+                    setShowMessage(true)
+
+                    setTimeout(() => {
+                        setShowMessage(false)
+                    }, 4000
+                    )
+                }
+
+            })
+            .catch(error => {
+                setMessage(`Tapahtui virhe. Tässä lisätietoa: ${error}`)
+                setIsPositive(false)
+                setShowMessage(true)
+
+                setTimeout(() => {
+                    setShowMessage(false)
+                }, 7000
+                )
+            })
+
+    setTimeout(() => {
+      setMuokkaustila(false)
+    }, 800
+    )
+        
+
+    }
+    // Komponentti palauttaa käyttöliittymään form elementin
+    // Lisätty required 2 ensimmäiseen inputiin
+
+    return (
+        <form onSubmit={submitLogin}>
+
+            {/* inputien tapahtumankäsittelijöissä on määritelty funktio, jotka saa parametrikseen kyseisen
+            input elementin target tiedon. Funktiot kutsuvat set state hookia parametrina target.value */}
+            <div>
+                <p style={{ color: 'white' }}>ID field cannot be edited</p>
+                <input type="text" value={newLoginId} />
+            </div>
+            <div>
+                <input type="text" value={newUsername} placeholder="Username"
+                    onChange={({ target }) => setNewUsername(target.value)} required />
+            </div>
+            <div>
+                <input type="text" value={newPassword} placeholder="Password"
+                    onChange={({ target }) => setNewPassword(target.value)} />
+            </div>
+            <div>
+                <input type="text" value={newFirstname} placeholder="Firstname"
+                    onChange={({ target }) => setNewFirstname(target.value)} />
+            </div>
+            <div>
+                <input type="text" value={newLastname} placeholder="Lastname"
+                    onChange={({ target }) => setNewLastname(target.value)} />
+            </div>
+            <div>
+                <input type="text" value={newEmail} placeholder="Email"
+                    onChange={({ target }) => setNewEmail(target.value)} />
+            </div>
+            <div>
+                <input type="text" value={newAccesslevelID} placeholder="AccesslevelID"
+                    onChange={({ target }) => setNewAccesslevelID(target.value)} />
+            </div>
+
+            <button className="nappi" type="submit" >Save</button>  
+            {/* poistettu: style={{ background: 'green' }} */}
+
+            <button className="nappi" onClick={() => setMuokkaustila(false)} style={{ background: 'red' }}>
+                Cancel</button>
+        </form>
+    )
+}
+
+export default LoginEdit
--- src/logins/LoginList.jsx
+++ src/logins/LoginList.jsx
@@ -3,6 +3,8 @@ import '../App.css'
 import LoginService from '../services/logins'
 import LoginAdd from './LoginAdd'
 import Login from './Login'
+import LoginEdit from './LoginEdit'     //tekeillä, backend puuttuu
+import Message from '../Message'
 
 const LoginList = () => {
 
@@ -10,6 +12,12 @@ const LoginList = () => {
   const [näytetäänkö, setNäytetäänkö] = useState(false)
   const [search, setSearch] = useState("")
   const [lisäysTila, setLisäystila] = useState(false)
+  const [muokkausTila, setMuokkaustila] = useState(false)
+  const [muokattavaLogin, setMuokattavaLogin] = useState({}) // yksi login olio
+
+  const [showMessage, setShowMessage] = useState(false)
+  const [isPositive, setIsPositive] = useState(false)
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     LoginService
@@ -18,7 +26,7 @@ const LoginList = () => {
       //console.log(data)
       setLogins(data)     
     })
-  }, [])
+  }, [lisäysTila, näytetäänkö, muokkausTila])
 
   //Hakukentän onChange tapahtumankäsittelijä
   const handleSeachInputChange = (event) => {
@@ -26,21 +34,91 @@ const LoginList = () => {
     setSearch(event.target.value.toLowerCase())
   }
 
+   const handleDeleteClick = id => {
+
+        //Kaivetaan esiin koko login olio jotta alertissa voidaan näyttää firstname id:n sijaan
+        const login = logins.find(cust => cust.loginId === id)
+
+        // Poiston varmistus kyselyikkuna
+        const confirm = window.confirm(`Haluatko todella poistaa: ${login.firstname}:n pysyvästi?`)
+
+        if (confirm) {
+
+            LoginService.remove(id)
+                .then(response => {
+
+                    if (response.status === 200) {
+                        // Poistetaan login statesta
+                        setLogins(logins.filter(filtered => filtered.loginId !== id))
+
+                        setMessage(`${login.firstname}:n poisto onnistui!`)
+                        setIsPositive(true)
+                        setShowMessage(true)
+                        window.scrollBy(0, -10000) // Scrollataan ylös jotta nähdään alert :)
+
+                        setTimeout(() => {
+                            setShowMessage(false)
+                        }, 4000
+                        )
+                    }
+
+                })
+
+                .catch(error => {
+                    console.log(error)
+                    setMessage(`Tapahtui virhe: ${error}. ?`)
+                    setIsPositive(false)
+                    setShowMessage(true)
+                    setNäytetäänkö(false)
+
+                    setTimeout(() => {
+                        setShowMessage(false)
+                    }, 7000
+                    )
+                })
+        }
+        else { // JOS KÄYTTÄJÄ EI VAHVISTANUT POISTOA:
+            setMessage('Poisto peruutettu')
+            setIsPositive(true)
+            setShowMessage(true)
+
+            setTimeout(() => {
+                setShowMessage(false)
+            }, 4000
+            )
+        }
+    }
+
+    //EDIT buttonin tapahtumankäsittelijä saa parametrin login componentista
+    const handleEditClick = login => {
+        //console.log(login)
+        setMuokattavaLogin(login)
+        setMuokkaustila(true)
+
+    }
+
   return(
       <>
 
-    <h1 style={{ cursor: 'pointer'}} 
-      onClick={() => setNäytetäänkö(!näytetäänkö)}>Logins
-    <button onClick={() => setLisäystila(true)}>Add new</button>
-    </h1>
+    <h1><nobr style={{ cursor: 'pointer'}} 
+      onClick={() => setNäytetäänkö(!näytetäänkö)}>Logins</nobr>
 
-    <input placeholder="Search by Firstname" value={search} onChange={handleSeachInputChange}/>
+    <button className="nappi" onClick={() => setLisäystila(true)}>Add new</button></h1>
+
+    {!lisäysTila && !muokkausTila &&
+       <input placeholder="Search by Firstname" value={search} onChange={handleSeachInputChange}/>
+    }
+
+    { showMessage &&
+        <Message message={message} isPositive={isPositive}/>
+    }
 
     {logins && näytetäänkö === true && lisäysTila === false && logins.map(login =>{
       const caseInsensName = login.firstname.toLowerCase()
         if (caseInsensName.indexOf(search) > -1) {
           return (
-            <Login key={login.loginId} login={login} />
+            <Login key={login.loginId} login={login} 
+              handleDeleteClick={handleDeleteClick} handleEditClick={handleEditClick}/>
           )
         }
       }   
@@ -48,7 +126,11 @@ const LoginList = () => {
     }
       {!logins && <p>Loading...</p>}
 
-      {lisäysTila === true && <LoginAdd setLisäystila={setLisäystila} />}
+      {lisäysTila === true && <LoginAdd setLisäystila={setLisäystila}  logins={logins} setLogins={setLogins} setMessage={setMessage} setShowMessage={setShowMessage}
+                setIsPositive={setIsPositive}/>}
+
+      {muokkausTila && <LoginEdit setMuokkaustila={setMuokkaustila} muokattavaLogin={muokattavaLogin} logins={logins} setLogins={setLogins} setMessage={setMessage} setShowMessage={setShowMessage}
+                setIsPositive={setIsPositive} />}
       </>
   )
 
